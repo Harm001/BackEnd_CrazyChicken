052aaed68037cddfe15a5dc6f73a42cec30962bb
CatWithPaws <valentunkulakov6@gmail.com>
2024-02-11 13:02:52 +0200

    a

--- Auction/AppDBContent.cs
+++ Auction/AppDBContent.cs
@@ -10,7 +10,8 @@ namespace Auction_TestTaskCrazyChicken_TestTaskCrazyChicken
     {
         public AppDBContent(DbContextOptions<AppDBContent> options) : base(options)
         {
-           
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
         }
 
         public DbSet<Auction> Auctions { get; set; }
